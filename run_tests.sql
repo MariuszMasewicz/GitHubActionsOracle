472eea64d142dc8f97705810dc4148e36e4627c6
Mariusz Masewicz <Mariusz.Masewicz@summ-it.pl>
2025-07-28 12:12:36 +0200

    Update run_tests.sql to specify reporters and output format; enhance test results upload in workflow

--- run_tests.sql
+++ run_tests.sql
@@ -1,9 +1,11 @@
 set echo on
 set timing on
 set serveroutput on
+SET SQLFORMAT ansiconsole
 
 -- Run all tests in the utPLSQL framework
 BEGIN 
-ut.run();
+ut.run(a_reporters => ut_junit_reporter(),
+       a_output => 'ut_results.xml');
 END;
 /
