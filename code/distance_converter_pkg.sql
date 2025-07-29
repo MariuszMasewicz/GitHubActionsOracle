f037cf1aa6c14c53efdebe4b87bb96f91485e1b9
Mariusz Masewicz <Mariusz.Masewicz@summ-it.pl>
2025-07-29 12:08:18 +0200

    Zaktualizuj pakiet konwertera odległości, dodając obsługę wyjątków dla ujemnych wartości oraz odpowiednie testy jednostkowe

--- code/distance_converter_pkg.sql
+++ code/distance_converter_pkg.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE PACKAGE distance_converter_pkg AS
+  distance_negative EXCEPTION;
   FUNCTION meters_to_yards(p_meters NUMBER) RETURN NUMBER;
   FUNCTION yards_to_meters(p_yards NUMBER) RETURN NUMBER;
   FUNCTION meters_to_feet(p_meters NUMBER) RETURN NUMBER;
@@ -11,43 +12,69 @@ END distance_converter_pkg;
 /
 
 CREATE OR REPLACE PACKAGE BODY distance_converter_pkg AS
+  distance_negative EXCEPTION;
+
   FUNCTION meters_to_yards(p_meters NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_meters < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_meters * 1.0936133;
   END meters_to_yards;
 
   FUNCTION yards_to_meters(p_yards NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_yards < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_yards / 1.0936133;
   END yards_to_meters;
 
   FUNCTION meters_to_feet(p_meters NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_meters < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_meters * 3.2808399;
   END meters_to_feet;
 
   FUNCTION feet_to_meters(p_feet NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_feet < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_feet / 3.2808399;
   END feet_to_meters;
 
   FUNCTION meters_to_inches(p_meters NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_meters < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_meters * 39.3700787;
   END meters_to_inches;
 
   FUNCTION inches_to_meters(p_inches NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_inches < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_inches / 39.3700787;
   END inches_to_meters;
 
   FUNCTION kilometers_to_miles(p_kilometers NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_kilometers < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_kilometers * 0.621371192;
   END kilometers_to_miles;
 
   FUNCTION miles_to_kilometers(p_miles NUMBER) RETURN NUMBER IS
   BEGIN
+    IF p_miles < 0 THEN
+      RAISE distance_negative;
+    END IF;
     RETURN p_miles / 0.621371192;
   END miles_to_kilometers;
 END distance_converter_pkg;
--- tests/distance_converter_pkg_test.sql
+++ tests/distance_converter_pkg_test.sql
@@ -4,20 +4,44 @@ CREATE OR REPLACE PACKAGE distance_converter_pkg_test IS
 
   --%test(Meters to Yards conversion)
   PROCEDURE test_meters_to_yards;
+  --%test(Meters to Yards negative distance)
+  PROCEDURE test_meters_to_yards_negative;
+
   --%test(Yards to Meters conversion)
   PROCEDURE test_yards_to_meters;
+  --%test(Yards to Meters negative distance)
+  PROCEDURE test_yards_to_meters_negative;
+
   --%test(Meters to Feet conversion)
   PROCEDURE test_meters_to_feet;
+  --%test(Meters to Feet negative distance)
+  PROCEDURE test_meters_to_feet_negative;
+
   --%test(Feet to Meters conversion)
   PROCEDURE test_feet_to_meters;
+  --%test(Feet to Meters negative distance)
+  PROCEDURE test_feet_to_meters_negative;
+
   --%test(Meters to Inches conversion)
   PROCEDURE test_meters_to_inches;
+  --%test(Meters to Inches negative distance)
+  PROCEDURE test_meters_to_inches_negative;
+
   --%test(Inches to Meters conversion)
   PROCEDURE test_inches_to_meters;
+  --%test(Inches to Meters negative distance)
+  PROCEDURE test_inches_to_meters_negative;
+
   --%test(Kilometers to Miles conversion)
   PROCEDURE test_kilometers_to_miles;
+  --%test(Kilometers to Miles negative distance)
+  PROCEDURE test_kilometers_to_miles_negative;
+
   --%test(Miles to Kilometers conversion)
   PROCEDURE test_miles_to_kilometers;
+  --%test(Miles to Kilometers negative distance)
+  PROCEDURE test_miles_to_kilometers_negative;
+
 END distance_converter_pkg_test;
 /
 
@@ -30,12 +54,26 @@ CREATE OR REPLACE PACKAGE BODY distance_converter_pkg_test IS
     ut.expect(l_result).to_be_between(1.0936, 1.0937);
   END test_meters_to_yards;
 
+  PROCEDURE test_meters_to_yards_negative IS
+  BEGIN
+    ut.expect(
+      distance_converter_pkg.meters_to_yards(-1)
+    ).to_raise_exception('DISTANCE_NEGATIVE');
+  END test_meters_to_yards_negative;
+
   PROCEDURE test_yards_to_meters IS
     l_result NUMBER;
   BEGIN
     l_result := distance_converter_pkg.yards_to_meters(1.0936133);
     ut.expect(l_result).to_be_between(0.9999, 1.0001);
   END test_yards_to_meters;
+
+  PROCEDURE test_yards_to_meters_negative IS
+  BEGIN
+    ut.expect(
+             distance_converter_pkg.yards_to_meters(-1)
+    ).to_raise_exception('DISTANCE_NEGATIVE');
+  END test_yards_to_meters_negative;
 
   PROCEDURE test_meters_to_feet IS
     l_result NUMBER;
@@ -44,12 +82,26 @@ CREATE OR REPLACE PACKAGE BODY distance_converter_pkg_test IS
     ut.expect(l_result).to_be_between(3.2808, 3.2809);
   END test_meters_to_feet;
 
+  PROCEDURE test_meters_to_feet_negative IS
+  BEGIN
+    ut.expect(
+      distance_converter_pkg.meters_to_feet(-1)
+    ).to_raise_exception('DISTANCE_NEGATIVE');
+  END test_meters_to_feet_negative;
+
   PROCEDURE test_feet_to_meters IS
     l_result NUMBER;
   BEGIN
     l_result := distance_converter_pkg.feet_to_meters(3.2808399);
     ut.expect(l_result).to_be_between(0.9999, 1.0001);
   END test_feet_to_meters;
+
+  PROCEDURE test_feet_to_meters_negative IS
+  BEGIN
+    ut.expect(
+     distance_converter_pkg.feet_to_meters(-1)
+    ).to_raise_exception('DISTANCE_NEGATIVE');
+  END test_feet_to_meters_negative;
 
   PROCEDURE test_meters_to_inches IS
     l_result NUMBER;
@@ -58,26 +110,16 @@ CREATE OR REPLACE PACKAGE BODY distance_converter_pkg_test IS
     ut.expect(l_result).to_be_between(39.3700, 39.3701);
   END test_meters_to_inches;
 
+  PROCEDURE test_meters_to_inches_negative IS
+  BEGIN
+    ut.expect(
+      distance_converter_pkg.meters_to_inches(-1)
+    ).to_raise_exception('DISTANCE_NEGATIVE');
+  END test_meters_to_inches_negative;
+
   PROCEDURE test_inches_to_meters IS
     l_result NUMBER;
   BEGIN
     l_result := distance_converter_pkg.inches_to_meters(39.3700787);
     ut.expect(l_result).to_be_between(0.9999, 1.0001);
-  END test_inches_to_meters;
-
-  PROCEDURE test_kilometers_to_miles IS
-    l_result NUMBER;
-  BEGIN
-    l_result := distance_converter_pkg.kilometers_to_miles(1);
-    ut.expect(l_result).to_be_between(0.6213, 0.6214);
-  END test_kilometers_to_miles;
-
-  PROCEDURE test_miles_to_kilometers IS
-    l_result NUMBER;
-  BEGIN
-    l_result := distance_converter_pkg.miles_to_kilometers(0.621371192);
-    ut.expect(l_result).to_be_between(0.9999, 1.0001);
-  END test_miles_to_kilometers;
-
-END distance_converter_pkg_test;
-/
+  END test_inches
